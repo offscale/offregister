04639f5ffbf3ee9ef548472049382b64dc5c6eec
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-09-15 21:22:39 -0400

    [setup.py] Update to latest Python 2&3 compatible format

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
+# -*- coding: utf-8 -*-
+
 from ast import parse
 from distutils.sysconfig import get_python_lib
 from functools import partial
+from operator import attrgetter, itemgetter
 from os import listdir, path
-from sys import version
+from sys import version_info
 
 from setuptools import find_packages, setup
 
-if version[0] == "2":
+if version_info[0] == 2:
     from itertools import ifilter as filter
     from itertools import imap as map
 
@@ -16,11 +19,23 @@ if __name__ == "__main__":
 
     with open(path.join(package_name, "__init__.py")) as f:
         __author__, __version__ = map(
-            lambda buf: next(map(lambda e: e.value.s, parse(buf).body)),
-            filter(
-                lambda line: line.startswith("__version__")
-                or line.startswith("__author__"),
-                f,
+            lambda const: const.value if version_info > (3, 6) else const.s,
+            map(
+                attrgetter("value"),
+                map(
+                    itemgetter(0),
+                    map(
+                        attrgetter("body"),
+                        map(
+                            parse,
+                            filter(
+                                lambda line: line.startswith("__version__")
+                                or line.startswith("__author__"),
+                                f,
+                            ),
+                        ),
+                    ),
+                ),
             ),
         )
 
