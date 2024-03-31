19f44da2af44a46f2c6dfe00a6ea8b5cf14c05d9
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2024-03-31 01:09:27 -0400

    [setup.py] Remove `distutils.sysconfig.get_python_lib` hack on `data_files` in favour of `package_data` ; confirm Python 3.13 support ; [README.md] Confirm Python 3.13 support

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup.py implementation, interesting because it parsed the first __init__.py and
 import sys
 from ast import Assign, Name, parse
 from functools import partial
+from itertools import chain
 from operator import attrgetter
 from os import listdir, path
 from os.path import extsep
@@ -15,88 +16,20 @@ from os.path import extsep
 from setuptools import find_packages, setup
 
 if sys.version_info[:2] >= (3, 12):
-    import os
-    from sysconfig import _BASE_EXEC_PREFIX as BASE_EXEC_PREFIX
-    from sysconfig import _BASE_PREFIX as BASE_PREFIX
-    from sysconfig import _EXEC_PREFIX as EXEC_PREFIX
-    from sysconfig import _PREFIX as PREFIX
-    from sysconfig import get_python_version
-
-    def is_virtual_environment():
-        """
-        Whether one is in a virtual environment
-        """
-        return sys.base_prefix != sys.prefix or hasattr(sys, "real_prefix")
-
-    def get_python_lib(plat_specific=0, standard_lib=0, prefix=None):
-        """Return the directory containing the Python library (standard or
-        site additions).
-
-        If 'plat_specific' is true, return the directory containing
-        platform-specific modules, i.e. any module from a non-pure-Python
-        module distribution; otherwise, return the platform-shared library
-        directory.  If 'standard_lib' is true, return the directory
-        containing standard Python library modules; otherwise, return the
-        directory for site-specific modules.
-
-        If 'prefix' is supplied, use it instead of sys.base_prefix or
-        sys.base_exec_prefix -- i.e., ignore 'plat_specific'.
-        """
-        is_default_prefix = not prefix or os.path.normpath(prefix) in (
-            "/usr",
-            "/usr/local",
-        )
-        if prefix is None:
-            if standard_lib:
-                prefix = plat_specific and BASE_EXEC_PREFIX or BASE_PREFIX
-            else:
-                prefix = plat_specific and EXEC_PREFIX or PREFIX
-
-        if os.name == "posix":
-            if plat_specific or standard_lib:
-                # Platform-specific modules (any module from a non-pure-Python
-                # module distribution) or standard Python library modules.
-                libdir = sys.platlibdir
-            else:
-                # Pure Python
-                libdir = "lib"
-            libpython = os.path.join(prefix, libdir, "python" + get_python_version())
-            if standard_lib:
-                return libpython
-            elif is_default_prefix and not is_virtual_environment():
-                return os.path.join(prefix, "lib", "python3", "dist-packages")
-            else:
-                return os.path.join(libpython, "site-packages")
-        elif os.name == "nt":
-            if standard_lib:
-                return os.path.join(prefix, "Lib")
-            else:
-                return os.path.join(prefix, "Lib", "site-packages")
-        else:
-
-            class DistutilsPlatformError(Exception):
-                """DistutilsPlatformError"""
-
-            raise DistutilsPlatformError(
-                "I don't know where Python installs its library "
-                "on platform '%s'" % os.name
-            )
-
     from ast import Del as Str
 else:
     from ast import Str
-    from distutils.sysconfig import get_python_lib
 
-if sys.version_info[0] == 2:
-    from itertools import ifilter as filter
-    from itertools import imap as map
+    if sys.version_info[0] == 2:
+        from itertools import ifilter as filter
+        from itertools import imap as map
 
 if sys.version_info[:2] > (3, 7):
     from ast import Constant
 else:
     from ast import expr
 
-    # Constant. Will never be used in Python =< 3.8
+    # Constant. Will never be used in Python < 3.8
     Constant = type("Constant", (expr,), {})
 
 
@@ -109,20 +42,17 @@ with open(
     long_description = fh.read()
 
 
-def to_funcs(*paths):
+def gen_join_on_pkg_name(*paths):
     """
-    Produce function tuples that produce the local and install dir, respectively.
+    Create a function that joins on `os.path.join` from the package name onward
 
     :param paths: one or more str, referring to relative folder names
     :type paths: ```*paths```
 
-    :return: 2 functions
-    :rtype: ```Tuple[Callable[Optional[List[str]], str], Callable[Optional[List[str]], str]]```
+    :return: function that joins on `os.path.join` from the package name onward
+    :rtype: ```Callable[tuple[str, ...], str]```
     """
-    return (
-        partial(path.join, path.dirname(__file__), package_name, *paths),
-        partial(path.join, get_python_lib(prefix=""), package_name, *paths),
-    )
+    return partial(path.join, path.dirname(__file__), package_name, *paths)
 
 
 def main():
@@ -160,9 +90,9 @@ def main():
         ),
     )
 
-    _data_join, _data_install_dir = to_funcs("_data")
-    _config_join, _config_install_dir = to_funcs("_config")
-    templates_join, templates_install_dir = to_funcs("aux_recipes", "templates")
+    _data_join = gen_join_on_pkg_name("_data")
+    _config_join = gen_join_on_pkg_name("_config")
+    templates_join = gen_join_on_pkg_name("aux_recipes", "templates")
 
     setup(
         name=package_name_verbatim,
@@ -193,21 +123,15 @@ def main():
             "Programming Language :: Python :: 3.10",
             "Programming Language :: Python :: 3.11",
             "Programming Language :: Python :: 3.12",
+            "Programming Language :: Python :: 3.13",
         ],
         license="(Apache-2.0 OR MIT OR CC0-1.0)",
         license_files=["LICENSE-APACHE", "LICENSE-MIT", "LICENSE-CC0"],
         test_suite="{}{}tests".format(package_name, path.extsep),
         packages=find_packages(),
-        package_dir={package_name: package_name},
         install_requires=["pyyaml", "apache-libcloud", "etcd3"],
-        data_files=[
-            (_data_install_dir(), list(map(_data_join, listdir(_data_join())))),
-            (_config_install_dir(), list(map(_config_join, listdir(_config_join())))),
-            (
-                templates_install_dir(),
-                list(map(templates_join, listdir(templates_join()))),
-            ),
-        ],
+        package_data={package_name: listdir(_data_join()) + listdir(_config_join()) + listdir(templates_join())},
+        include_package_data=True,
     )
 
 
